27ab917e9663df77e03d3e4c72107908055a1008
anyushi <434624198@qq.com>
2022-06-13 14:36:48 +0900

    ----重新增加ReRAM分布----

--- test/normal.py
+++ test/normal.py
@@ -9,7 +9,15 @@ import linecache
 import numpy as np
 import pandas as pd
 
-hs = np.random.normal(1.2628, 0.8325, 33000)
+lognormal_mean = 457
+lognormal_std = 2.323
+
+
+normal_std = np.sqrt(np.log(1 + (lognormal_std/lognormal_mean)**2))
+normal_mean = np.log(lognormal_mean) - normal_std**2 / 2
+
+hs = np.random.normal(2.6, 0.366, 300000)
+
 
 print("hs.max()", hs.max())  # some finite number
 print("hs.mean()", hs.mean())  # about 136519
